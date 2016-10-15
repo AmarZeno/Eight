f20061039133a3fe8233765d197d63d5b7791039
Zeno Saviourmarsalin <u1077160@ad.utah.edu>
2016-10-15 01:10:23 -0600

    Added drag feature

---
+++ Assets/Scripts/mouseDrag.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+
+public class mouseDrag : MonoBehaviour, IDragHandler
+{
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z);
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = objPosition;
+    }
+
+}
