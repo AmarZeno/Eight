2c3fbb73b8895a2fd079e32b57f17b06bc01342b
u1077160 <zeno.saviour@utah.edu>
2016-10-18 16:54:33 -0600

    Combine script work in progress

---
+++ Assets/Scripts/SceneOneCombineScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneOneCombineScript : MonoBehaviour {
+
+    public GameObject elementContainer;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Combine(other.gameObject);
+    }
+
+    void Combine(GameObject element) {
+        elementContainer.transform.localPosition = gameObject.transform.localPosition;
+        gameObject.transform.SetParent(element.transform);
+        element.transform.SetParent(elementContainer.transform);
+
+        //IList<Transform> transforms = new IList<Transform>();
+        
+        //foreach(Transform child in transform) {
+        //    transforms.Add(child);
+        //}
+
+        //foreach (Transform child in transforms) {
+        //    child = element;
+        //}
+    }
+}
--- Assets/Scripts/screenWrap.cs
+++ Assets/Scripts/screenWrap.cs
@@ -11,37 +11,33 @@ public class screenWrap : MonoBehaviour {
     }
 
     void ScreenWrap() {
+
         // Update local position if game object moves beyond the canvas(Also include offset for gameObject centre anchor point)
+        // 1) Take the difference of the position that bounced over the boundary
+        // 2) Subtract the difference to reposition the object inside the boundary
+
         // Left boundary check
         if ((gameObject.transform.localPosition.x - gameObject.GetComponent<RectTransform>().rect.width / 2) < -(canvas.GetComponent<RectTransform>().rect.width / 2))
-        {
-            // Take the difference of the position that bounced over the boundary
+        {    
             float difference = (gameObject.transform.localPosition.x - gameObject.GetComponent<RectTransform>().rect.width / 2) - (-canvas.GetComponent<RectTransform>().rect.width / 2);
-            // Subtract the difference to reposition the object inside the boundary
             transform.localPosition = new Vector2(gameObject.transform.localPosition.x - difference, transform.localPosition.y);
         } 
         // Right Boundary Check
         else if((gameObject.transform.localPosition.x + gameObject.GetComponent<RectTransform>().rect.width / 2) > canvas.GetComponent<RectTransform>().rect.width / 2)
         {
-            // Take the difference of the position that bounced over the boundary
             float difference = (gameObject.transform.localPosition.x + gameObject.GetComponent<RectTransform>().rect.width / 2) - canvas.GetComponent<RectTransform>().rect.width / 2;
-            // Subtract the difference to reposition the object inside the boundary
             transform.localPosition = new Vector2(gameObject.transform.localPosition.x - difference, transform.localPosition.y);
         }
         // Top boundary check
         else if ((gameObject.transform.localPosition.y + gameObject.GetComponent<RectTransform>().rect.height / 2) > canvas.GetComponent<RectTransform>().rect.height / 2)
         {
-            // Take the difference of the position that bounced over the boundary
             float difference = (gameObject.transform.localPosition.y + gameObject.GetComponent<RectTransform>().rect.height / 2) - canvas.GetComponent<RectTransform>().rect.height / 2;
-            // Subtract the difference to reposition the object inside the boundary
             transform.localPosition = new Vector2(transform.localPosition.x, gameObject.transform.localPosition.y - difference);
         }
         // Bottom boundary check
         else if ((gameObject.transform.localPosition.y - gameObject.GetComponent<RectTransform>().rect.height / 2) < (-canvas.GetComponent<RectTransform>().rect.height / 2))
         {
-            // Take the difference of the position that bounced over the boundary
             float difference = (gameObject.transform.localPosition.y - gameObject.GetComponent<RectTransform>().rect.height / 2) - (-canvas.GetComponent<RectTransform>().rect.height / 2);
-            // Subtract the difference to reposition the object inside the boundary
             transform.localPosition = new Vector2(transform.localPosition.x, gameObject.transform.localPosition.y - difference);
         }
 
