88c802e8a1414438eb32413892ca72390ec4831e
u1077160 <zeno.saviour@utah.edu>
2016-10-18 15:41:48 -0600

    Screen wrap stable

--- Assets/Scripts/screenWrap.cs
+++ Assets/Scripts/screenWrap.cs
@@ -3,44 +3,47 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class screenWrap : MonoBehaviour {
-    float leftConstraint = 0.0f;
-    float rightConstraint = 960.0f;
-    float buffer = 10.0f; // set this so the spaceship disappears offscreen before re-appearing on other side
 
-    private Image image;
     public Canvas canvas;
-    void Start() {
-        // set Vector3 to ( camera left/right limits, spaceship Y, spaceship Z )
-        // this will find a world-space point that is relative to the screen
-        //   leftConstraint = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width * 0.05f, 0.0f, 0.0f)).x; // Or set to (0,0,0)
-        //   rightConstraint = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width * 0.95f, 0.0f, 0.0f)).x; // Or set to (Screen.width,0,0)
-        leftConstraint = - Screen.width/2;
-        rightConstraint = Screen.width/2;
-         image =  gameObject.GetComponent<Image>();
-    }
 
-    // Update is called once per frame
     void Update () {
-        //ScreenWrap();
-
-        if(gameObject.GetComponent<Image>().transform.localPosition.x < -(canvas.transform.position.))
-        {
-            gameObject.GetComponent<Image>().transform.localPosition= Vector2.zero;
-        }
+        ScreenWrap();
     }
 
     void ScreenWrap() {
-        if (transform.position.x < leftConstraint - buffer) { 
-            transform.position = new Vector2(rightConstraint + buffer, transform.position.y);
+        // Update local position if game object moves beyond the canvas(Also include offset for gameObject centre anchor point)
+        // Left boundary check
+        if ((gameObject.transform.localPosition.x - gameObject.GetComponent<RectTransform>().rect.width / 2) < -(canvas.GetComponent<RectTransform>().rect.width / 2))
+        {
+            // Take the difference of the position that bounced over the boundary
+            float difference = (gameObject.transform.localPosition.x - gameObject.GetComponent<RectTransform>().rect.width / 2) - (-canvas.GetComponent<RectTransform>().rect.width / 2);
+            // Subtract the difference to reposition the object inside the boundary
+            transform.localPosition = new Vector2(gameObject.transform.localPosition.x - difference, transform.localPosition.y);
+        } 
+        // Right Boundary Check
+        else if((gameObject.transform.localPosition.x + gameObject.GetComponent<RectTransform>().rect.width / 2) > canvas.GetComponent<RectTransform>().rect.width / 2)
+        {
+            // Take the difference of the position that bounced over the boundary
+            float difference = (gameObject.transform.localPosition.x + gameObject.GetComponent<RectTransform>().rect.width / 2) - canvas.GetComponent<RectTransform>().rect.width / 2;
+            // Subtract the difference to reposition the object inside the boundary
+            transform.localPosition = new Vector2(gameObject.transform.localPosition.x - difference, transform.localPosition.y);
+        }
+        // Top boundary check
+        else if ((gameObject.transform.localPosition.y + gameObject.GetComponent<RectTransform>().rect.height / 2) > canvas.GetComponent<RectTransform>().rect.height / 2)
+        {
+            // Take the difference of the position that bounced over the boundary
+            float difference = (gameObject.transform.localPosition.y + gameObject.GetComponent<RectTransform>().rect.height / 2) - canvas.GetComponent<RectTransform>().rect.height / 2;
+            // Subtract the difference to reposition the object inside the boundary
+            transform.localPosition = new Vector2(transform.localPosition.x, gameObject.transform.localPosition.y - difference);
+        }
+        // Bottom boundary check
+        else if ((gameObject.transform.localPosition.y - gameObject.GetComponent<RectTransform>().rect.height / 2) < (-canvas.GetComponent<RectTransform>().rect.height / 2))
+        {
+            // Take the difference of the position that bounced over the boundary
+            float difference = (gameObject.transform.localPosition.y - gameObject.GetComponent<RectTransform>().rect.height / 2) - (-canvas.GetComponent<RectTransform>().rect.height / 2);
+            // Subtract the difference to reposition the object inside the boundary
+            transform.localPosition = new Vector2(transform.localPosition.x, gameObject.transform.localPosition.y - difference);
         }
 
-        if (transform.position.x > rightConstraint + buffer) {
-            transform.position = new Vector2(leftConstraint - buffer, transform.position.y);
-        }
-        
     }
-
-
-
-
 }
