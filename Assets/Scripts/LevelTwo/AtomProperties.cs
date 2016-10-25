7f1293ca2513af6ab63faef5f3e3c47c8c2ef85b
Zeno Saviourmarsalin <u1077160@ad.utah.edu>
2016-10-25 02:00:34 -0600

    Level two work in progress

---
+++ Assets/Scripts/LevelTwo/AtomProperties.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AtomProperties : MonoBehaviour {
+    public enum AtomBondingState { Unknown, Successful, Failed};
+
+    public AtomBondingState flourineOneBondState = AtomBondingState.Unknown;
+    public AtomBondingState flourineTwoBondState = AtomBondingState.Unknown;
+    public AtomBondingState flourineThreeBondState = AtomBondingState.Unknown;
+    public AtomBondingState flourineFourBondState = AtomBondingState.Unknown;
+}
---
+++ Assets/Scripts/LevelTwo/LevelTwoBond.cs
@@ -0,0 +1,156 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+
+public class LevelTwoBond : mouseDrag {
+
+    // Constants
+    readonly Vector2 fluorineAtomLinearOffset = new Vector2(0, 14);
+
+    // Variables
+    public GameObject atomProperties;
+
+
+    private GameObject draggedAtom = null;
+    private bool isBondingFailed = false;
+    private AtomProperties atomPropertiesScript;
+
+    void Start() {
+        atomPropertiesScript = atomProperties.GetComponent<AtomProperties>();
+    }
+
+    public override void OnDrag(PointerEventData eventData)
+    {
+        base.OnDrag(eventData);
+        draggedAtom = eventData.pointerDrag.gameObject;
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        Bond(other);
+    }
+
+    void Bond(Collider2D otherAtomCollider) {
+
+        // Prevent bonding when the user is not dragging an atom and when he makes a wrong bonding
+        if (draggedAtom == null || isBondingFailed == true)
+            return;
+
+        GameObject collidedAtom = otherAtomCollider.GetComponent<Collider2D>().gameObject;
+
+        // Handle five collision cases
+        // 1) When the dragged atom collides with Fluorine1 atom
+        // 2) When the dragged atom collides with Fluorine2 atom
+        // 3) When the dragged atom collides with Fluorine3 atom
+        // 4) When the dragged atom collides with Fluorine4 atom
+        // 5) When the dragged atom collides with Carbon atom
+        switch (collidedAtom.name) {
+            case "Fluorine1":
+                {
+                    // Break if already made a succesful bond
+                    if (atomPropertiesScript.flourineOneBondState == AtomProperties.AtomBondingState.Successful || atomPropertiesScript.flourineOneBondState == AtomProperties.AtomBondingState.Failed)
+                        break;
+
+                    if (draggedAtom.name == "Fluorine2" || draggedAtom.name == "Fluorine3" || draggedAtom.name == "Fluorine4")
+                    {
+                        collidedAtom.GetComponent<RelativeJoint2D>().connectedBody = draggedAtom.GetComponent<Rigidbody2D>();
+                        collidedAtom.GetComponent<RelativeJoint2D>().linearOffset = fluorineAtomLinearOffset;
+                        isBondingFailed = true;
+                        atomPropertiesScript.flourineOneBondState = AtomProperties.AtomBondingState.Failed;
+                    }
+                    else {
+                        atomPropertiesScript.flourineOneBondState = AtomProperties.AtomBondingState.Successful;
+                    }
+                    // Enable bonding joint for colliding with any type of atom
+                    collidedAtom.GetComponent<RelativeJoint2D>().enabled = true;
+                }
+                break;
+            case "Fluorine2":
+                {
+                    // Break if already made a succesful bond
+                    if (atomPropertiesScript.flourineTwoBondState == AtomProperties.AtomBondingState.Successful || atomPropertiesScript.flourineTwoBondState == AtomProperties.AtomBondingState.Failed)
+                        break;
+
+                    if (draggedAtom.name == "Fluorine1" || draggedAtom.name == "Fluorine3" || draggedAtom.name == "Fluorine4")
+                    {
+                        collidedAtom.GetComponent<RelativeJoint2D>().connectedBody = draggedAtom.GetComponent<Rigidbody2D>();
+                        collidedAtom.GetComponent<RelativeJoint2D>().linearOffset = fluorineAtomLinearOffset;
+                        isBondingFailed = true;
+                        atomPropertiesScript.flourineTwoBondState = AtomProperties.AtomBondingState.Failed;
+                    }
+                    else {
+                        atomPropertiesScript.flourineTwoBondState = AtomProperties.AtomBondingState.Successful;
+                    }
+                    // Enable bonding joint for colliding with any type of atom
+                    collidedAtom.GetComponent<RelativeJoint2D>().enabled = true;
+                }
+                break;
+            case "Fluorine3":
+                {
+                    // Break if already made a succesful bond
+                    if (atomPropertiesScript.flourineThreeBondState == AtomProperties.AtomBondingState.Successful || atomPropertiesScript.flourineThreeBondState == AtomProperties.AtomBondingState.Failed)
+                        break;
+
+                    if (draggedAtom.name == "Fluorine1" || draggedAtom.name == "Fluorine2" || draggedAtom.name == "Fluorine4")
+                    {
+                        collidedAtom.GetComponent<RelativeJoint2D>().connectedBody = draggedAtom.GetComponent<Rigidbody2D>();
+                        collidedAtom.GetComponent<RelativeJoint2D>().linearOffset = fluorineAtomLinearOffset;
+                        isBondingFailed = true;
+                        atomPropertiesScript.flourineThreeBondState = AtomProperties.AtomBondingState.Failed;
+                    }
+                    else {
+                        atomPropertiesScript.flourineThreeBondState = AtomProperties.AtomBondingState.Successful;
+                    }
+                    // Enable bonding joint for colliding with any type of atom
+                    collidedAtom.GetComponent<RelativeJoint2D>().enabled = true;
+                }
+                break;
+            case "Fluorine4":
+                {
+                    // Break if already made a succesful bond
+                    if (atomPropertiesScript.flourineFourBondState == AtomProperties.AtomBondingState.Successful || atomPropertiesScript.flourineFourBondState == AtomProperties.AtomBondingState.Failed)
+                        break;
+
+                    if (draggedAtom.name == "Fluorine1" || draggedAtom.name == "Fluorine2" || draggedAtom.name == "Fluorine3")
+                    {
+                        collidedAtom.GetComponent<RelativeJoint2D>().connectedBody = draggedAtom.GetComponent<Rigidbody2D>();
+                        collidedAtom.GetComponent<RelativeJoint2D>().linearOffset = fluorineAtomLinearOffset;
+                        isBondingFailed = true;
+                        atomPropertiesScript.flourineFourBondState = AtomProperties.AtomBondingState.Failed;
+                    }
+                    else {
+                        atomPropertiesScript.flourineFourBondState = AtomProperties.AtomBondingState.Successful;
+                    }
+                    // Enable bonding joint for colliding with any type of atom
+                    collidedAtom.GetComponent<RelativeJoint2D>().enabled = true;
+                }
+                break;
+            case "Carbon":
+                draggedAtom.GetComponent<RelativeJoint2D>().enabled = true;
+                switch (draggedAtom.name) {
+                    case "Fluorine1":
+                        atomPropertiesScript.flourineOneBondState = AtomProperties.AtomBondingState.Successful;
+                        break;
+                    case "Fluorine2":
+                        atomPropertiesScript.flourineTwoBondState = AtomProperties.AtomBondingState.Successful;
+                        break;
+                    case "Fluorine3":
+                        atomPropertiesScript.flourineThreeBondState = AtomProperties.AtomBondingState.Successful;
+                        break;
+                    case "Fluorine4":
+                        atomPropertiesScript.flourineFourBondState = AtomProperties.AtomBondingState.Successful;
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            default:
+                // Do nothing
+                break;
+        }
+
+        // Reset draggedAtom
+        draggedAtom = null;
+    }
+    
+}
