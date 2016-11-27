e0184a4b93db971a3e16b6e56d3159239fffc259
Zeno <amarzeno@outlook.com>
2016-11-27 03:10:30 -0700

    Background music added

---
+++ Assets/Scripts/SingletonPersist.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SingletonPersist : MonoBehaviour {
+
+    private static SingletonPersist instance;
+
+    public static SingletonPersist GetInstance() {
+        return instance;
+    }
+
+	// Use this for initialization
+	void Awake () {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+}
