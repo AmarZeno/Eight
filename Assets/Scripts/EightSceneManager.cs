bf6e96db211723517ae0976a10f27c97b8f53b83
Zeno <amarzeno@outlook.com>
2016-11-27 01:42:31 -0700

    Added starting scene

---
+++ Assets/Scripts/EightSceneManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class EightSceneManager : MonoBehaviour {
+
+    public void LoadScene(int level) {
+        SceneManager.LoadScene(level);
+    }
+
+    public void Exit() {
+        Application.Quit();
+    }
+}
