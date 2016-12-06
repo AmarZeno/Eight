42052625c97efb1f975a436557de14457e98df9b
Zeno <amarzeno@outlook.com>
2016-12-06 01:11:33 -0700

    Work in progress - Split Atoms

--- Assets/Scripts/SplitAtoms.cs
+++ Assets/Scripts/SplitAtoms.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class SplitAtoms : MonoBehaviour {
 
@@ -8,29 +9,119 @@ public class SplitAtoms : MonoBehaviour {
 
 	public LayerMask touchInputMask;
 
-	// Use this for initialization
-	void Start () {
+    private List<GameObject> touchList = new List<GameObject>();
+    private GameObject[] touchesOld;
+
+    private RaycastHit hit;
+
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.touchCount == 2) {
+
+#if UNITY_EDITOR
+        if (Input.GetMouseButton(0) || Input.GetMouseButtonDown(0) || Input.GetMouseButtonUp(0))
+        {
+
+            touchesOld = new GameObject[touchList.Count];
+            touchList.CopyTo(touchesOld);
+            touchList.Clear();
+
+
+            Ray ray2 = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray2, out hit, touchInputMask))
+            {
+                GameObject recipient2 = hit.transform.gameObject;
+                touchList.Add(recipient2);
+
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    // recipient.GetComponent<RelativeJoint2D>().enabled = false;
+                    recipient2.SendMessage("OnTouchDown", hit.point, SendMessageOptions.DontRequireReceiver);
+                }
+
+                if (Input.GetMouseButtonUp(0))
+                {
+                    recipient2.SendMessage("OnTouchUp", hit.point, SendMessageOptions.DontRequireReceiver);
+                }
+
+                if (Input.GetMouseButton(0))
+                {
+                    recipient2.SendMessage("OnTouchStay", hit.point, SendMessageOptions.DontRequireReceiver);
+                }
+
+                //if(recipient.transform.name == "Hydrogen2" || recipient.transform.name == "Hydrogen1"){
+
+                //	hydrogenFour.GetComponent<RelativeJoint2D> ().enabled = false;
+
+                //	hydrogenTwo.GetComponent<RelativeJoint2D> ().enabled = false;
+
+                //}
+
+                foreach (GameObject g in touchesOld)
+                {
+                    if (!touchList.Contains(g))
+                    {
+                        g.SendMessage("OnTouchExit", hit.point, SendMessageOptions.DontRequireReceiver);
+                    }
+                }
+            }
+        }
+#endif
+
+            if (Input.touchCount == 2) {
+
+            touchesOld = new GameObject[touchList.Count];
+            touchList.CopyTo(touchesOld);
+            touchList.Clear();
 
 			foreach (Touch touch in Input.touches) {
-				Ray ray1 = Camera.main.ScreenPointToRay(touch);
-				RaycastHit hit;
+				Ray ray1 = Camera.main.ScreenPointToRay(touch.position);
+				
 				if (Physics.Raycast (ray1, out hit, touchInputMask)) {
 					GameObject recipient =  hit.transform.gameObject;
-					if(recipient.transform.name == "Hydrogen2" || recipient.transform.name == "Hydrogen1"){
+                    touchList.Add(recipient);
+
+
+                    if (touch.phase == TouchPhase.Began) {
+                         recipient.GetComponent<RelativeJoint2D>().enabled = false;
+                        recipient.SendMessage("OnTouchDown", hit.point, SendMessageOptions.DontRequireReceiver);
+                    }
+
+                    if (touch.phase == TouchPhase.Ended) {
+                        recipient.SendMessage("OnTouchUp", hit.point, SendMessageOptions.DontRequireReceiver);
+                    }
+
+                    if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved) {
+                        Vector3 objPosition = Camera.main.ScreenToWorldPoint(touch.position);
+                        recipient.gameObject.transform.position = objPosition;
+                        recipient.SendMessage("OnTouchStay", hit.point, SendMessageOptions.DontRequireReceiver);
+                    }
+
+                    if (touch.phase == TouchPhase.Canceled) {
+                        recipient.SendMessage("OnTouchExit", hit.point, SendMessageOptions.DontRequireReceiver);
+                    }
+
+					//if(recipient.transform.name == "Hydrogen2" || recipient.transform.name == "Hydrogen1"){
 						
-						hydrogenFour.GetComponent<RelativeJoint2D> ().enabled = false;
+					//	hydrogenFour.GetComponent<RelativeJoint2D> ().enabled = false;
 
-						hydrogenTwo.GetComponent<RelativeJoint2D> ().enabled = false;
+					//	hydrogenTwo.GetComponent<RelativeJoint2D> ().enabled = false;
 
-					}
+					//}
 				}
 			}
+
+            foreach (GameObject g in touchesOld) {
+                if (!touchList.Contains(g)) {
+                    g.SendMessage("OnTouchExit", hit.point, SendMessageOptions.DontRequireReceiver);
+                }
+            }
 
 			//TODO Fix split atoms
 			/*RaycastHit2D firstHitInformation = Physics2D.Raycast(hydrogenTwo.transform.position, Camera.main.transform.forward);
