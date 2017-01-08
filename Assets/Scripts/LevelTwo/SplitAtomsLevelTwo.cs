ea9eecbf4c05984de64b864be73eeb6f0f9bfe23
Amar Zeno <Amar Zeno>
2017-01-08 01:21:50 -0700

    Added success split for level two

--- Assets/Scripts/LevelTwo/SplitAtomsLevelTwo.cs
+++ Assets/Scripts/LevelTwo/SplitAtomsLevelTwo.cs
@@ -67,7 +67,7 @@ public class SplitAtomsLevelTwo : MonoBehaviour
                 LevelTwoAtomProperties.AtomBondingState state = atomPropertiesScript.flourineAtomListStates[(Convert.ToInt32(tappedGameObject.name[tappedGameObject.name.Length - 1].ToString()) - 1)];
 
                 // Prevent splitting for unknown states
-                if (state == LevelTwoAtomProperties.AtomBondingState.Unknown || state == LevelTwoAtomProperties.AtomBondingState.Successful) {
+                if (state == LevelTwoAtomProperties.AtomBondingState.Unknown) {
                     return;
                 }
 
@@ -78,7 +78,14 @@ public class SplitAtomsLevelTwo : MonoBehaviour
                     if (tappedGameObject.GetComponent<RelativeJoint2D>().enabled == true)
                     {
                         // If the tapped gameobject has an enabled Joint then continue splitting that Atom
-                        SplitAtom(tappedGameObject);
+                        if (state == LevelTwoAtomProperties.AtomBondingState.Successful) // Split success bonds
+                        {
+                            SplitSuccessBond(tappedGameObject);
+                        }
+                        else // Split wrong bonds
+                        {
+                            SplitAtom(tappedGameObject);
+                        }
                     }
                     else
                     {
@@ -113,7 +120,7 @@ public class SplitAtomsLevelTwo : MonoBehaviour
                     // Prevent splitting for unknown states
                     LevelTwoAtomProperties.AtomBondingState state = atomPropertiesScript.flourineAtomListStates[(Convert.ToInt32(tappedGameObject.name[tappedGameObject.name.Length - 1].ToString()) - 1)];
 
-                    if (state == LevelTwoAtomProperties.AtomBondingState.Unknown || state == LevelTwoAtomProperties.AtomBondingState.Successful)
+                    if (state == LevelTwoAtomProperties.AtomBondingState.Unknown)
                     {
                         return;
                     }
@@ -125,7 +132,14 @@ public class SplitAtomsLevelTwo : MonoBehaviour
                         if (tappedGameObject.GetComponent<RelativeJoint2D>().enabled == true)
                         {
                             // If the tapped gameobject has an enabled Joint then continue splitting that Atom
-                            SplitAtom(tappedGameObject);
+                            if (state == LevelTwoAtomProperties.AtomBondingState.Successful) // Split success bonds
+                            {
+                                SplitSuccessBond(tappedGameObject);
+                            }
+                            else // Split wrong bonds
+                            {
+                                SplitAtom(tappedGameObject);
+                            }
                         }
                         else {
                             // Check for gameObject that holds joint with the tapped gameobject
@@ -140,6 +154,34 @@ public class SplitAtomsLevelTwo : MonoBehaviour
                 }
             }
         }
+    }
+
+    void SplitSuccessBond(GameObject tappedGameObject) {
+        GameObject connectedGameObject = tappedGameObject.GetComponent<RelativeJoint2D>().connectedBody.gameObject;
+        tappedGameObject.GetComponent<RelativeJoint2D>().enabled = false;
+
+        if (connectedGameObject.transform.localPosition.x - tappedGameObject.transform.localPosition.x < 10 && connectedGameObject.transform.localPosition.x - tappedGameObject.transform.localPosition.x > -10) // Lies top or bottom
+        {
+            if (tappedGameObject.transform.localPosition.y > connectedGameObject.transform.localPosition.y)
+            { // TOP
+                tappedGameObject.transform.localPosition = new Vector3(tappedGameObject.transform.localPosition.x, tappedGameObject.transform.localPosition.y + 70, tappedGameObject.transform.localPosition.z);
+            }
+            else { // BOTTOM
+                tappedGameObject.transform.localPosition = new Vector3(tappedGameObject.transform.localPosition.x, tappedGameObject.transform.localPosition.y - 70, tappedGameObject.transform.localPosition.z);
+            }
+        }
+        else { // Lies left or right
+            if (tappedGameObject.transform.localPosition.x < connectedGameObject.transform.localPosition.x) // LEFT
+            {
+                tappedGameObject.transform.localPosition = new Vector3(tappedGameObject.transform.localPosition.x - 70, tappedGameObject.transform.localPosition.y, tappedGameObject.transform.localPosition.z);
+            }
+            else { // RIGHT
+                tappedGameObject.transform.localPosition = new Vector3(tappedGameObject.transform.localPosition.x + 70, tappedGameObject.transform.localPosition.y, tappedGameObject.transform.localPosition.z);
+            }
+        }
+        AssociateDefaultPositions(tappedGameObject);
+        tappedGameObject.GetComponent<MouseDrag>().enabled = true;
+        tappedGameObject.GetComponent<LevelTwoBond>().enabled = true;
     }
 
     void SplitAtom(GameObject tappedGameObject) {
